07e5bdbab0161a27782dcfabb2ecfdee01f224c2
Mike Nedviga <109431070+DidItHard@users.noreply.github.com>
2023-04-15 17:37:59 +0300

    fix singIn (Scroll)

--- lib/pages/auth/sign_in_page.dart
+++ lib/pages/auth/sign_in_page.dart
@@ -34,151 +34,153 @@ class SignInPage extends StatelessWidget {
           },
           builder: (context, state) {
             return SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 38),
-                child: Column(
-                  children: <Widget>[
-                    // Flexible(flex: 2, child: Container()),
-                    const SizedBox(
-                      height: 54,
-                    ),
-                    Image.asset(
-                      'assets/images/signIn-page/Logo.png',
-                      height: 76,
-                    ),
-                    const SizedBox(
-                      height: 22,
-                    ),
-                    Text(
-                      'Log In',
-                      style: TextStyle(
-                          fontFamily: 'Comfortaa',
-                          fontSize: 38,
-                          fontWeight: FontWeight.w900,
-                          color: Colors.white),
-                    ),
-                    // Flexible(flex: 1, child: Container()),
-                    const SizedBox(
-                      height: 192,
-                    ),
-                    PrimaryTextField(
-                      controller: _textFormLoginController,
-                      labelText: 'Enter your Username...',
-                      labelStyle: TextStyle(),
-                      validator: (e) {},
-                    ),
-                    const SizedBox(
-                      height: 22,
-                    ),
-                    PrimaryTextField(
-                      controller: _textFormPasswordController,
-                      labelText: 'Enter your Password...',
-                      labelStyle: TextStyle(),
-                      validator: (e) {},
-                      isPasswordField: true,
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 8),
-                      width: 120,
-                      child: Column(
-                        children: const [
-                          Divider(
-                            thickness: 1,
-                            height: 8,
-                            color: AppColors.color12D18E,
-                          ),
-                          Divider(
-                            thickness: 1,
-                            height: 8,
-                            color: AppColors.color12D18E,
-                          ),
-                        ],
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 38),
+                  child: Column(
+                    children: <Widget>[
+                      // Flexible(flex: 2, child: Container()),
+                      const SizedBox(
+                        height: 54,
                       ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: AppColors.color12D18E,
+                      Image.asset(
+                        'assets/images/signIn-page/Logo.png',
+                        height: 76,
                       ),
-                      child: Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          onTap: () async {
-                            // signIn
-                            context.read<SignInBloc>().add(
-                                  SignInAuth(
-                                    login: _textFormLoginController.text,
-                                    password: _textFormPasswordController.text,
-                                  ),
-                                );
-                          },
-                          child: Container(
-                            height: 50,
-                            child: Center(
-                              child: const Text(
-                                'Continue',
-                                style: TextStyle(
-                                    fontFamily: 'VarelaRound',
-                                    fontSize: 20,
-                                    color: AppColors.averageGrey),
+                      const SizedBox(
+                        height: 22,
+                      ),
+                      Text(
+                        'Log In',
+                        style: TextStyle(
+                            fontFamily: 'Comfortaa',
+                            fontSize: 38,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white),
+                      ),
+                      // Flexible(flex: 1, child: Container()),
+                      const SizedBox(
+                        height: 192,
+                      ),
+                      PrimaryTextField(
+                        controller: _textFormLoginController,
+                        labelText: 'Enter your Username...',
+                        labelStyle: TextStyle(),
+                        validator: (e) {},
+                      ),
+                      const SizedBox(
+                        height: 22,
+                      ),
+                      PrimaryTextField(
+                        controller: _textFormPasswordController,
+                        labelText: 'Enter your Password...',
+                        labelStyle: TextStyle(),
+                        validator: (e) {},
+                        isPasswordField: true,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 8),
+                        width: 120,
+                        child: Column(
+                          children: const [
+                            Divider(
+                              thickness: 1,
+                              height: 8,
+                              color: AppColors.color12D18E,
+                            ),
+                            Divider(
+                              thickness: 1,
+                              height: 8,
+                              color: AppColors.color12D18E,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: AppColors.color12D18E,
+                        ),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: () async {
+                              // signIn
+                              context.read<SignInBloc>().add(
+                                    SignInAuth(
+                                      login: _textFormLoginController.text,
+                                      password: _textFormPasswordController.text,
+                                    ),
+                                  );
+                            },
+                            child: Container(
+                              height: 50,
+                              child: Center(
+                                child: const Text(
+                                  'Continue',
+                                  style: TextStyle(
+                                      fontFamily: 'VarelaRound',
+                                      fontSize: 20,
+                                      color: AppColors.averageGrey),
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 22,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: AppColors.colorD7FFCA,
+                      const SizedBox(
+                        height: 22,
                       ),
-                      child: Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          onTap: () async {
-                            // signIn
-                            context.read<SignInBloc>().add(
-                                  SignInAuth(
-                                    login: _textFormLoginController.text,
-                                    password: _textFormPasswordController.text,
-                                  ),
-                                );
-                          },
-                          child: Container(
-                            height: 50,
-                            child: Center(
-                              child: const Text(
-                                'Go Back',
-                                style: TextStyle(
-                                    fontFamily: 'VarelaRound',
-                                    fontSize: 20,
-                                    color: Colors.black),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: AppColors.colorD7FFCA,
+                        ),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: () async {
+                              // signIn
+                              context.read<SignInBloc>().add(
+                                    SignInAuth(
+                                      login: _textFormLoginController.text,
+                                      password: _textFormPasswordController.text,
+                                    ),
+                                  );
+                            },
+                            child: Container(
+                              height: 50,
+                              child: Center(
+                                child: const Text(
+                                  'Go Back',
+                                  style: TextStyle(
+                                      fontFamily: 'VarelaRound',
+                                      fontSize: 20,
+                                      color: Colors.black),
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    // Container(
-                    //   child: Center(
-                    //     child: CupertinoButton(
-                    //       onPressed: () {
-                    //         context.go(Routes.signUpPage);
-                    //       },
-                    //       child: Text(
-                    //         "Регистрация",
-                    //         style: TextStyle(
-                    //           color: AppColors.color7e56e8,
-                    //         ),
-                    //       ),
-                    //     ),
-                    //   ),
-                    // ),
-                    Flexible(flex: 3, child: Container()),
-                  ],
+                      // Container(
+                      //   child: Center(
+                      //     child: CupertinoButton(
+                      //       onPressed: () {
+                      //         context.go(Routes.signUpPage);
+                      //       },
+                      //       child: Text(
+                      //         "Регистрация",
+                      //         style: TextStyle(
+                      //           color: AppColors.color7e56e8,
+                      //         ),
+                      //       ),
+                      //     ),
+                      //   ),
+                      // ),
+                      Flexible(flex: 3, child: Container()),
+                    ],
+                  ),
                 ),
               ),
             );
