510ffe0f635278e61373ea0e783cda353da962ba
Egor Mizulin <egormizulin@mail.ru>
2023-04-15 14:59:36 +0300

    add ways2share page

---
+++ lib/pages/ways_to_share/ways_to_share_page.dart
@@ -0,0 +1,10 @@
+import 'package:/flutter/material.dart';
+
+class WayToSharePage extends StatelessWidget {
+  const WayToSharePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
